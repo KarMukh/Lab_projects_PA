8f36077deec945e38e131360a19643e9e9d0f94b
KarMukh <kmukhsyan@gmail.com>
2023-02-10 02:18:41 +0400

    Add file

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
 
 @app.route("/")
-def hi():
-    return "<p>Hi!</p>"
-
-@app.route("/")
-def ho():
-    return "<p>Hol!</p>"
+def les():
+    return "<p>les!</p>"
