25b2da92981ee551a71a9ff7232bcd7ab7b5f7c4
KarMukh <kmukhsyan@gmail.com>
2023-02-10 19:35:29 +0400

    Add file

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template, url_for, redirect
+
 
 app = Flask(__name__)
+app.app_context().push()
 
 
-@app.route("/")
-def les():
-    return "<p>les!</p>"
+@app.route('/')
+def index():
+	return render_template('form.html')
+
+
+@app.route('/execute', methods=['GET', 'POST'])
+def execute():
+	*********************************************
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
+
